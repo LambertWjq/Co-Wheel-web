398c8ce3217633011a616f0247d182688a74819b
Baron <68785875+Byzantine-blackthird@users.noreply.github.com>
2023-09-26 20:04:40 +0800

    文件上传
    新增文件上传

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -3,11 +3,24 @@ import { ref } from 'vue'
 import type { TabsPaneContext } from 'element-plus'
 import WorkProjectCard, {type WorkProjectProps} from '@/components/card/WorkProjectCard.vue';
 import fileTable from '@/components/FileTable.vue';
+import {Plus, Upload} from "@element-plus/icons-vue";
+import { UploadFilled } from '@element-plus/icons-vue'
 
 const activeName = ref('project')
+const dialogVisible = ref(false)
+const file = ref(null)
+
 
 const handleClick = (tab: TabsPaneContext, event: Event) => {
   console.log(tab, event)
+}
+
+const showDialog = () => {
+  dialogVisible.value = true;
+}
+
+const uploadFile = () => {
+  dialogVisible.value = false;
 }
 
 </script>
@@ -21,6 +34,43 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
       <file-table></file-table>
     </el-tab-pane>
   </el-tabs>
+  <el-button
+      style="position: absolute; right: 16px;top: 70px"
+      :icon="Plus"
+  >
+    新建
+  </el-button>
+  <el-button
+      style="position: absolute; right: 104px;top: 70px"
+      :icon="Upload"
+      @click="showDialog"
+  >
+    上传
+  </el-button>
+  <el-dialog v-model="dialogVisible" title="上传文件">
+    <el-upload
+        class="upload-demo"
+        drag
+        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        multiple
+    >
+      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <div class="el-upload__text">
+        Drop file here or <em>click to upload</em>
+      </div>
+      <template #tip>
+        <div class="el-upload__tip">
+          jpg/png files with a size less than 500kb
+        </div>
+      </template>
+    </el-upload>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="uploadFile">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <style scoped>
